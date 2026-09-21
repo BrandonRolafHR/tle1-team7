8e21d35b9573aa98686c654d529261ce48cf8f77
ChrisvL <chrisvanloenen7@gmail.com>
2026-09-21 10:45:30 +0200

    Fonts setup

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
           name="viewport">
     <meta content="ie=edge" http-equiv="X-UA-Compatible">
-    <title>MediPod</title>
-    <link rel="icon" type="image/x-icon" href="/media/favicon.gif">
+    <title></title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.gif">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
