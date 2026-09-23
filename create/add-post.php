ba74f7f3b422cd1284db06d2a6825927f8525c71
ChrisvL <chrisvanloenen7@gmail.com>
2026-09-23 10:46:30 +0200

    prompt for choice added

---
+++ create/add-post.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+          name="viewport">
+    <meta content="ie=edge" http-equiv="X-UA-Compatible">
+    <title></title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.gif">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/create.css">
+</head>
+<body>
+<main>
+
+</main>
+
+<?php require_once "../components/footer.php"; ?>
+</body>
